cc78cb34685e0f0bf8cce71728c18fb6a62db8e6
sun4lower <sun4lower@sun4lower.cn>
2018-01-25 23:10:08 +0800

    add sitemap at the bottom of Home page

--- usr/themes/mytheme/footer.php
+++ usr/themes/mytheme/footer.php
@@ -38,6 +38,7 @@
                     	<p>Theme is <a href="https://github.com/chakhsu/pinghsu" target="_blank">Pinghsu</a> by <a href="https://www.linpx.com/" target="_blank">Chakhsu</a></p>
 						<p>Powered by <a href="http://www.typecho.org" target="_blank" rel="nofollow">Typecho</a></p>
 						<p>&copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a></p>
+                                                <p><a href="/index.php/sitemap.html">网站地图</a></p>
 					</div>
 				</div>
 			</div>
